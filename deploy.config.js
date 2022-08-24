26be976d31cb154ada8f3d7dc0be947f0ce7b44c
hasianamin <hasianamin14@gmail.com>
2022-08-25 00:31:55 +0700

    edit lagi

--- deploy.config.js
+++ deploy.config.js
@@ -9,17 +9,17 @@ module.exports = {
       },
       time: true,
     },
-    {
-      name: 'JCWD-2104-01-FE',
-      script: 'npm',
-      args: 'start',
-      cwd: '/var/www/html/jcwd-2104-01.purwadhikabootcamp.com',
-      instances: 'max',
-      exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3401,
-      },
-    },
+    // {
+    //   name: 'JCWD-2104-01-FE',
+    //   script: 'npm',
+    //   args: 'start',
+    //   cwd: '/var/www/html/jcwd-2104-01.purwadhikabootcamp.com',
+    //   instances: 'max',
+    //   exec_mode: 'cluster',
+    //   env: {
+    //     NODE_ENV: 'production',
+    //     PORT: 3401,
+    //   },
+    // },
   ],
 };
