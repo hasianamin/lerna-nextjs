95ef5cc779fdb3bbf508e61b3114cd1656f9da34
hasianamin <hasianamin14@gmail.com>
2022-08-25 09:07:49 +0700

    edit

--- deploy.config.js
+++ deploy.config.js
@@ -13,7 +13,7 @@ module.exports = {
       name: 'JCWD-2104-01-FE',
       script: 'npm',
       args: 'start',
-      cwd: '/var/www/html/jcwd-210401.purwadhikabootcamp.com/packages/client',
+      cwd: '/packages/client',
       instances: 'max',
       exec_mode: 'cluster',
       env: {
